b75b320e1d953440fd0376cc69ba326247862b1d
Tursunkhuja <tursunkhuja@go.alif.academy>
2022-12-26 19:28:53 +0500

    Fixed issue with not showing Fields & Functions on "Edit Value" window of Web Report Designer

--- SampleServerApp/Program.cs
+++ SampleServerApp/Program.cs
@@ -5,10 +5,10 @@ using TelerikReportingRestService.Controllers;
 Console.WriteLine("Starting Reporting and OData endpoints!");
 
 var endpointManager = new EndpointManager();
-var endpointTelerikReporting = new AppEndpoint(TelerikReportingRestService.Startup.ConfigureServices, TelerikReportingRestService.Startup.Configure,  "/Reporting");
-endpointManager.AddAppEndpoint(endpointTelerikReporting, typeof(ReportsController).Assembly);
-
 var endpointTest = new AppEndpoint(ODataService.ODataStartup.ConfigureServices, ODataService.ODataStartup.Configure, path: "/OData");
 endpointManager.AddAppEndpoint(endpointTest, typeof(ODataService.Controllers.WeatherForecastController).Assembly);
+
+var endpointTelerikReporting = new AppEndpoint(TelerikReportingRestService.Startup.ConfigureServices, TelerikReportingRestService.Startup.Configure,  "/Reporting");
+endpointManager.AddAppEndpoint(endpointTelerikReporting, typeof(ReportsController).Assembly);
 
 endpointManager.OpenCommunications(false);
